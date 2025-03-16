abc0d04334aa1e3c4812be19f8381480ca556877
WAHYUAKHMADFADILLAH <wahyufadil1140@gmail.com>
2025-03-17 01:28:53 +0700

    seo optimi

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata, Viewport } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
+import Script from 'next/script'
 import { Header } from './header'
 import { Footer } from './footer'
 import { ThemeProvider } from 'next-themes'
@@ -27,10 +28,19 @@ export const metadata: Metadata = {
     siteName: 'Fullstack Web Developer - Wahyu Akhmad Fadillah',
     locale: 'en_US',
     type: 'website',
+    images: [
+      {
+        url: `${WEBSITE_URL}/profile.png`,
+        width: 1200,
+        height: 630,
+        alt: 'Wahyu Akhmad Fadillah - Fullstack Developer',
+      },
+    ],
   },
   alternates: {
     canonical: WEBSITE_URL,
   },
+
   icons: {
     icon: '/profile.svg',
     apple: '/apple-touch-icon.png',
@@ -47,13 +57,15 @@ export const metadata: Metadata = {
       'max-snippet': -1,
     },
   },
+  keywords:
+    'fullstack developer, web development, javascript, react, next.js, node.js',
   twitter: {
     card: 'summary_large_image',
     title: 'Fullstack Web Developer - Wahyu Akhmad Fadillah',
     description:
       'Passionate Fullstack Developer crafting high-performance, intuitive, and stunning web experiences. Expert in JavaScript, React, Next.js & Node.js',
-    site: '@yourTwitterHandle',
-    creator: '@yourTwitterHandle',
+    site: '@wahyuakhmadfad3',
+    creator: '@wahyuakhmadfad3',
     images: [`${WEBSITE_URL}/profile.png`],
   },
 }
@@ -75,6 +87,28 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      // Di dalam fungsi RootLayout, tambahkan di dalam tag{' '}
+      <head>
+        <Script
+          id="json-ld"
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              '@context': 'https://schema.org',
+              '@type': 'Person',
+              name: 'Wahyu Akhmad Fadillah',
+              url: WEBSITE_URL,
+              jobTitle: 'Fullstack Web Developer',
+              knowsAbout: ['JavaScript', 'React', 'Next.js', 'Node.js'],
+              sameAs: [
+                'https://github.com/fadilsflow',
+                'https://linkedin.com/in/wahyu-akhmad-fadillah',
+                'https://twitter.com/wahyuakhmadfad3',
+              ],
+            }),
+          }}
+        />
+      </head>
       <body
         className={`${geist.variable} ${geistMono.variable} bg-white tracking-tight antialiased dark:bg-zinc-950`}
       >

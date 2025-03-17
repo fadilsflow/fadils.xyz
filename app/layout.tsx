b74a08a116ec8cb292ac29b1f45544fe69f0daea
WAHYUAKHMADFADILLAH <wahyufadil1140@gmail.com>
2025-03-18 00:27:39 +0700

    ga

--- app/layout.tsx
+++ app/layout.tsx
@@ -40,8 +40,9 @@ export const metadata: Metadata = {
   alternates: {
     canonical: WEBSITE_URL,
   },
+
   icons: {
-    icon: '/favicon.ico',
+    icon: '/favicon.svg',
     apple: '/apple-touch-icon.png',
   },
   manifest: '/site.webmanifest',
@@ -87,7 +88,6 @@ const geist = Geist({
   variable: '--font-geist',
   subsets: ['latin'],
 })
-
 const geistMono = Geist_Mono({
   variable: '--font-geist-mono',
   subsets: ['latin'],
@@ -101,16 +101,79 @@ export default function RootLayout({
   return (
     <html lang="id" suppressHydrationWarning>
       <head>
-        {/* Ikon untuk berbagai perangkat */}
-        <link rel="icon" href="/favicon.ico" sizes="any" />
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
-        <link rel="manifest" href="/site.webmanifest" />
-
-        {/* Metadata tambahan */}
+        <link
+          rel="apple-touch-icon"
+          sizes="57x57"
+          href="/apple-icon-57x57.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="60x60"
+          href="/apple-icon-60x60.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="72x72"
+          href="/apple-icon-72x72.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="76x76"
+          href="/apple-icon-76x76.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="114x114"
+          href="/apple-icon-114x114.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="120x120"
+          href="/apple-icon-120x120.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="144x144"
+          href="/apple-icon-144x144.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="152x152"
+          href="/apple-icon-152x152.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-icon-180x180.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="192x192"
+          href="/android-icon-192x192.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="96x96"
+          href="/favicon-96x96.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/manifest.json" />
         <meta name="msapplication-TileColor" content="#ffffff" />
+        <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
         <meta name="theme-color" content="#ffffff" />
-
-        {/* Structured Data (JSON-LD) */}
         <Script
           id="json-ld"
           type="application/ld+json"
@@ -120,7 +183,7 @@ export default function RootLayout({
               '@type': 'Person',
               name: 'Wahyu Akhmad Fadillah',
               url: WEBSITE_URL,
-              image: `${WEBSITE_URL}/profile.png`,
+              image: 'https://fadils.xyz/profile.png',
               jobTitle: 'Fullstack Web Developer',
               knowsAbout: ['JavaScript', 'React', 'Next.js', 'Node.js'],
               sameAs: [
@@ -141,17 +204,13 @@ export default function RootLayout({
           storageKey="theme"
           defaultTheme="system"
         >
-          <div className="flex min-h-screen w-full flex-col">
-            {/* Header */}
-            <Header />
-
-            {/* Konten utama */}
-            <main className="mx-auto w-full max-w-screen-sm flex-1 px-4 pt-20">
+          <div className="flex min-h-screen w-full flex-col font-[family-name:var(--font-inter-tight)]">
+            <div className="relative mx-auto w-full max-w-screen-sm flex-1 px-4 pt-20">
+              <Header />
               {children}
-            </main>
 
-            {/* Footer */}
-            <Footer />
+              <Footer />
+            </div>
           </div>
         </ThemeProvider>
       </body>
